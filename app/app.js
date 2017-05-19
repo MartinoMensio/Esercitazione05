dd70b7fadfe7bd52b11ff6a45b10f5d11b1c6c57
Martino Mensio <martinomensio@outlook.it>
2017-05-19 16:49:05 +0200

    show leaflet map

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('App', ['ngRoute', 'ngResource'])
+var app = angular.module('App', ['ngRoute', 'ngResource', 'ui-leaflet'])
 
 app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
