ef1bdb77f00894eaa6b819356b1a85ec4e446c7e
aleciav <ale.ciav92@gmail.com>
2017-05-22 17:05:38 +0200

    Enabling the bus line visualization

--- app/app.js
+++ app/app.js
@@ -7,6 +7,11 @@ app.config(function ($routeProvider, $locationProvider) {
             controller: 'MainCtrl',
             controllerAs: 'ctrl'
         })
+        .when('/:lineId', {
+            templateUrl: 'templates/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'ctrl'
+        })
         .otherwise({ redirectTo: "/" });
 
     // configure html5 to get links working on jsfiddle
--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,8 +1,27 @@
 var app = angular.module('App');
 
-app.controller('MainCtrl', ['$scope', 'DataProvider',
-    function ($scope, DataProvider) {
+app.controller('MainCtrl', ['$scope', '$routeParams', '$location', 'leafletBoundsHelpers', 'DataProvider',
+    function ($scope, $routeParams, $location, leafletBoundsHelpers,  DataProvider) {
+        var lineId = $routeParams.lineId;
         this.lines = DataProvider.getLines();
-        this.geojson = DataProvider.geojson();
+
+        this.center = {
+            lat: 45.064,
+            lng: 7.681,
+            zoom: 13
+        }
+
+        if (lineId) {
+            this.geojson = DataProvider.getLineByIdAsGeoJson(lineId);
+            
+            this.mapBounds = leafletBoundsHelpers.createBoundsFromArray(this.geojson);
+        }
+        else {
+
+        }
+
+        this.showLine = function(lineId) {
+            $location.path(lineId);
+        }
     }
 ]);
