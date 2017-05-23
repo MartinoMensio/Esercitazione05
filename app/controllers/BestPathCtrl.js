3e8af393e834b0cdde697e6670f77db554478ca5
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-23 19:09:43 +0200

    Making the marker druggable

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -4,39 +4,40 @@ app.controller('BestPathCtrl', ['$scope', '$routeParams', '$location', 'leafletD
 function ($scope, $routeParams, $location, leafletData,  DataProvider) {
 
 	angular.extend($scope, {
-			center: {
+		center: {
+			lat: 45.064,
+			lng: 7.681,
+			zoom: 13
+		},
+
+		markers: {
+			m1: {
 				lat: 45.064,
 				lng: 7.681,
-				zoom: 13
+				message: "I'm the center of Turin",
+				draggable : true
 			},
+		},
 
-        markers: {
-            m1: {
-                lat: 45.064,
-				lng: 7.681,
-                message: "I'm the center of Turin",
-            },
-        },
-
-    });
+	});
 
 	/* COME CAZZO E' POSSIBILE CHE QUELLO SOPRA VA E QUESTO NO? */
 	/* nell'html ho sostituito lf-center="ctrl.center" con l'attributo center="center" */
 	/*this.center = {
-		lat: 44.064,
-		lng: 7.681,
-		zoom: 13
-	}
+	lat: 44.064,
+	lng: 7.681,
+	zoom: 13
+}
 
-	markers: {
-		m1: {
-			lat: 44.064,
-			lng: 7.681,
-			message: "I'm the center of Turin",
-		}
-	}
-	*/
-	/* center variable describes the starting point on the map to be visualized */
+markers: {
+m1: {
+lat: 44.064,
+lng: 7.681,
+message: "I'm the center of Turin",
+}
+}
+*/
+/* center variable describes the starting point on the map to be visualized */
 
 
 
