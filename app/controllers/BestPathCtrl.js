d36ae62bf37f1a3ab5ba34536472e8df8eb33f9f
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-23 19:03:47 +0200

    Test Marker - HELP!

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -1,15 +1,43 @@
 var app = angular.module('App');
 
 app.controller('BestPathCtrl', ['$scope', '$routeParams', '$location', 'leafletData', 'DataProvider',
-    function ($scope, $routeParams, $location, leafletData,  DataProvider) {
-       $scope.welcomeMessage = 'Welcome user to the best path page!';
-	   /*var lineId = $routeParams.lineId;*/
-	   this.lines = DataProvider.getLines();
+function ($scope, $routeParams, $location, leafletData,  DataProvider) {
 
-	   this.center = {
-		   lat: 45.064,
-		   lng: 7.681,
-		   zoom: 13
-	   }
-    }
-]);
+	angular.extend($scope, {
+			center: {
+				lat: 45.064,
+				lng: 7.681,
+				zoom: 13
+			},
+
+        markers: {
+            m1: {
+                lat: 45.064,
+				lng: 7.681,
+                message: "I'm the center of Turin",
+            },
+        },
+
+    });
+
+	/* COME CAZZO E' POSSIBILE CHE QUELLO SOPRA VA E QUESTO NO? */
+	/* nell'html ho sostituito lf-center="ctrl.center" con l'attributo center="center" */
+	/*this.center = {
+		lat: 44.064,
+		lng: 7.681,
+		zoom: 13
+	}
+
+	markers: {
+		m1: {
+			lat: 44.064,
+			lng: 7.681,
+			message: "I'm the center of Turin",
+		}
+	}
+	*/
+	/* center variable describes the starting point on the map to be visualized */
+
+
+
+}]);
