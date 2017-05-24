45a09cf05f4c6d95251b672a1e8fbde5da68b04b
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-24 09:56:06 +0200

    Getting lat/lng from a marker druggable

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -9,17 +9,31 @@ function ($scope, $routeParams, $location, leafletData,  DataProvider) {
 			lng: 7.681,
 			zoom: 13
 		},
-
+		defaults: {
+            scrollWheelZoom: false
+        },
 		markers: {
 			m1: {
 				lat: 45.064,
 				lng: 7.681,
+				focus: true,
 				message: "I'm the center of Turin",
 				draggable : true
 			},
 		},
+		events: {
+			map: {
+				enable: ['click', 'drag', 'blur', 'touchstart'],
+				logic: 'emit'
+			}
+		},
 
 	});
+
+	$scope.$on("leafletDirectiveMarker.dragend", function(event, args){
+			   $scope.position.lat = args.model.lat;
+			   $scope.position.lng = args.model.lng;
+		   });
 
 	/* COME CAZZO E' POSSIBILE CHE QUELLO SOPRA VA E QUESTO NO? */
 	/* nell'html ho sostituito lf-center="ctrl.center" con l'attributo center="center" */
