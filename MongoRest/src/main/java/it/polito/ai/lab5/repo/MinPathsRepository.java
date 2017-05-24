662b65b59773b76bf61a9e387ee68655412a6890
Martino Mensio <martinomensio@outlook.it>
2017-05-24 22:53:02 +0200

    enabling CORS server side and changing port

--- MongoRest/src/main/java/it/polito/ai/lab5/repo/MinPathsRepository.java
+++ MongoRest/src/main/java/it/polito/ai/lab5/repo/MinPathsRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.polito.ai.lab5.entities.Key;
 import it.polito.ai.lab5.entities.MinPath;
 
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "min_paths", path = "min_paths")
 public interface MinPathsRepository extends MongoRepository<MinPath, Key> {
 	/**
