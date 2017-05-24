b7e7d43262f5c37ac2b2fafa81d2f148df695cde
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-24 10:20:46 +0200

    Adding source and destination markers, removing useless functions

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -13,46 +13,52 @@ function ($scope, $routeParams, $location, leafletData,  DataProvider) {
             scrollWheelZoom: false
         },
 		markers: {
-			m1: {
+			src: {
+				lat: 45.064,
+				lng: 7.685,
+				focus: true,
+				message: "I'm the path source",
+				draggable : true
+			},
+			dst: {
 				lat: 45.064,
 				lng: 7.681,
 				focus: true,
-				message: "I'm the center of Turin",
+				message: "I'm the path destination",
 				draggable : true
-			},
-		},
-		events: {
-			map: {
-				enable: ['click', 'drag', 'blur', 'touchstart'],
-				logic: 'emit'
 			}
 		},
+		events: {
+			/*map: {
+				enable: ['click', 'drag', 'blur', 'touchstart'],
+				logic: 'emit'
+			}*/
+		}
+
+
+	});
+	/*
+	$scope.markers = new Array();
+
+	$scope.$on("leafletDirectiveMap.click", function(event, args){
+		var leafEvent = args.leafletEvent;
+
+			   $scope.markers.push({
+							 lat: leafEvent.latlng.lat,
+							 lng: leafEvent.latlng.lng,
+							 message: "My Added Marker"
+				});
 
 	});
 
-	$scope.$on("leafletDirectiveMarker.dragend", function(event, args){
+/*	$scope.$on("leafletDirectiveMarker.dragend", function(event, args){
 			   $scope.position.lat = args.model.lat;
 			   $scope.position.lng = args.model.lng;
-		   });
 
-	/* COME CAZZO E' POSSIBILE CHE QUELLO SOPRA VA E QUESTO NO? */
-	/* nell'html ho sostituito lf-center="ctrl.center" con l'attributo center="center" */
-	/*this.center = {
-	lat: 44.064,
-	lng: 7.681,
-	zoom: 13
-}
 
-markers: {
-m1: {
-lat: 44.064,
-lng: 7.681,
-message: "I'm the center of Turin",
-}
-}
-*/
-/* center variable describes the starting point on the map to be visualized */
+		   });*/
 
 
 
-}]);
+
+} ]);
