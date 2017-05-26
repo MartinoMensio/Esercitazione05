4168e600b35333e664b2e389870ea2df59cca9bc
Riccardo Persiani <s225289@studenti.polito.it>
2017-05-26 16:24:28 +0200

    Adding legend test

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -19,7 +19,12 @@ function ($scope, leafletMapEvents, $routeParams, $location, leafletData,  DataP
 				enable: ['zoomstart', 'drag', 'click', 'mousemove'],
 				logic: 'emit'
 			}
-		}
+		},
+		legend: {
+                    position: 'bottomleft',
+                    colors: [ '#ff0000', '#28c9ff', '#0000ff', '#ecf386' ],
+                    labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
+                },
 	});
 
 	$scope.markers = new Array();
@@ -44,6 +49,8 @@ function ($scope, leafletMapEvents, $routeParams, $location, leafletData,  DataP
 
    $scope.removeMarkers = function() {
 	   $scope.markers = new Array();
+	   $scope.geojson = 0;
+
    }
 
    $scope.findPath = function() {
