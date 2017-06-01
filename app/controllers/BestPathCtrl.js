31570631d19e384fc8d72b55e2b96bf3ad6d2b89
Martino Mensio <martinomensio@outlook.it>
2017-06-01 12:48:29 +0200

    cleanup of controller removed unused event and ES6 "this"

--- app/controllers/BestPathCtrl.js
+++ app/controllers/BestPathCtrl.js
@@ -2,8 +2,6 @@ var app = angular.module('App');
 
 app.controller('BestPathCtrl', ['$scope', "leafletMapEvents", '$routeParams', '$location', 'MinPathProvider', 'GeocodingService',
     function ($scope, leafletMapEvents, $routeParams, $location, MinPathProvider, GeocodingService) {
-        // save reference to the controller for functions that execute out of scope
-        var self = this;
 
         // initialize the map
         this.center = {
@@ -33,59 +31,52 @@ app.controller('BestPathCtrl', ['$scope', "leafletMapEvents", '$routeParams', '$
         };
 
         // handle user adding a marker
-        $scope.$on("leafletDirectiveMap.click", function (event, args) {
+        $scope.$on("leafletDirectiveMap.click", (event, args) => {
             // check how many markers are there on the map
-            if (Object.keys(self.markers).length < 2) {
+            if (Object.keys(this.markers).length < 2) {
                 // decide if this is the source (first marker) or the destination (second marker)
-                var key = Object.keys(self.markers).length == 0 ? 'source' : 'destination';
+                var key = Object.keys(this.markers).length == 0 ? 'source' : 'destination';
                 var leafEvent = args.leafletEvent;
                 // build the marker
-                self.markers[key] = {
+                this.markers[key] = {
                     lat: leafEvent.latlng.lat,
                     lng: leafEvent.latlng.lng,
                     message: key,
                     draggable: true
                 };
                 // update the search form
-                self[key + 'Str'] = 'set on map';
+                this[key + 'Str'] = 'set on map';
             }
         });
 
-        $scope.$on("leafletDirectiveMarker.dragend", function (event, args) {
-            console.log('hola');
-            // already updated!!
-            //$scope.markers[0].lat = args.model.lat;
-            //$scope.markers[0].lng = args.model.lng;
-        });
-
-        this.removeMarkers = function () {
-            self.markers = {};
-            self.geojson = [];
-            self.sourceStr = '';
-            self.destinationStr = '';
+        this.removeMarkers = () => {
+            this.markers = {};
+            this.geojson = [];
+            this.sourceStr = '';
+            this.destinationStr = '';
         }
 
         // use the service to get the suggested path
-        this.findPath = function () {
-            var source = self.markers['source'];
-            var destination = self.markers['destination'];
+        this.findPath = () => {
+            var source = this.markers['source'];
+            var destination = this.markers['destination'];
             if (source && destination) {
-                MinPathProvider.getMinPathBetween(source, destination, true).then(function (result) {
-                    self.geojson = result.geojson;
-                    self.markers = result.markers;
+                MinPathProvider.getMinPathBetween(source, destination, true).then((result) => {
+                    this.geojson = result.geojson;
+                    this.markers = result.markers;
                 });
             }
         }
 
-        this.geocodeSrc = function () {
-            GeocodingService.getLocationFromString(self.sourceStr).then(function (result) {
-                self.markers['source'] = result.geometry.location;
+        this.geocodeSrc = () => {
+            GeocodingService.getLocationFromString(this.sourceStr).then((result) => {
+                this.markers['source'] = result.geometry.location;
             })
         }
 
-        this.geocodeDst = function () {
-            GeocodingService.getLocationFromString(self.destinationStr).then(function (result) {
-                self.markers['destination'] = result.geometry.location;
+        this.geocodeDst = () => {
+            GeocodingService.getLocationFromString(this.destinationStr).then((result) => {
+                this.markers['destination'] = result.geometry.location;
             })
         }
     }
